9603872597c49e68e4b48a04a10e95c2422d6112
Digital House Brasil <99281226+PedagogiaDHBrasil@users.noreply.github.com>
2022-09-13 16:02:43 -0300

    Update utils.test.ts

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,8 +1,14 @@
 import { formatUserName } from "./utils";
 
 describe('formatUserName', () => {
-   test("formatUserName adiciona @ no início do nome de usuário", () => {});
+   test("formatUserName adiciona @ no início do nome de usuário", () => {
+     const result = formatUserName("rick");
+     expect(result).toBe("@rick");
+   });
 
-   test("formatUserName não adiciona @ quando já é fornecido", () => {});
+   test("formatUserName não adiciona @ quando já é fornecido", () => {
+     const result = formatUserName("@rick");
+     expect(result).toBe("@rick");
+   });
 });
  
